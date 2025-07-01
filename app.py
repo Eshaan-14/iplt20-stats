e28ea34592af81b2073650cdb6a9b7f626d5f71e
Eshaan-14 <eshaanmane954386@gmail.com>
2025-07-01 18:30:32 +0000

    save to deploy

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
+st.title("Eshaan's First Streamlit App")
 st.write("Here's Eshaan's sss second attempt at using data to create a table:")
 # df = pd.DataFrame({
 #   'first column': [1, 2, 3, 4],
@@ -8,16 +10,30 @@ st.write("Here's Eshaan's sss second attempt at using data to create a table:")
 
 # df
 df = pd.read_csv('./matches.csv')
+ 
+st.write(df.shape)
 
-df.shape
-df_match['yr']=df_match.date.str[:4]
-df_match.head()
-df_match.pivot_table(index='yr',columns=['winner'],aggfunc='count',values='venue').fillna('')
-alt.Chart(df_match).mark_circle().encode(
+df['yr']=df.date.str[:4]
+df.head()
+
+col=st.multiselect("Col",df.columns,default=['winner'])
+row=st.multiselect("Row",df.columns,default=['yr'])
+
+df_piv1=df.pivot_table( index=row, 
+                        columns=col, 
+                        aggfunc='count',
+                        values='venue'
+                    ).fillna('')
+st.write(df_piv1)
+
+
+chart1 =alt.Chart(df).mark_circle().encode(
     x='winner',
     y='yr',
     size='count()',
     color='yr',
     tooltip=['count()']
     )
+
+st.write(chart1)
 
