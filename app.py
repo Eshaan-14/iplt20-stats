5a67e3481baa5f23d78af73eca5a510b6832fbbf
Eshaan-14 <eshaanmane954386@gmail.com>
2025-07-01 18:38:41 +0000

    tabs added

--- app.py
+++ app.py
@@ -16,24 +16,38 @@ st.write(df.shape)
 df['yr']=df.date.str[:4]
 df.head()
 
-col=st.multiselect("Col",df.columns,default=['winner'])
-row=st.multiselect("Row",df.columns,default=['yr'])
 
-df_piv1=df.pivot_table( index=row, 
-                        columns=col, 
-                        aggfunc='count',
-                        values='venue'
-                    ).fillna('')
-st.write(df_piv1)
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    col=st.multiselect("Col",df.columns,default=['winner'])
+    row=st.multiselect("Row",df.columns,default=['yr'])
+
+    df_piv1=df.pivot_table( index=row, 
+                            columns=col, 
+                            aggfunc='count',
+                            values='venue'
+                        ).fillna('')
+    st.write(df_piv1)
 
 
-chart1 =alt.Chart(df).mark_circle().encode(
-    x='winner',
-    y='yr',
-    size='count()',
-    color='yr',
-    tooltip=['count()']
-    )
 
-st.write(chart1)
+
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    chart1 =alt.Chart(df).mark_circle().encode(
+        x='winner',
+        y='yr',
+        size='count()',
+        color='yr',
+        tooltip=['count()']
+        )
+
+    st.write(chart1)
 
